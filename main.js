cefc43e5d415182fe27decbfd5607272e2bc06dd
TheSweGoose <105603049+TheSweGoose@users.noreply.github.com>
2022-08-08 11:00:41 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -24,24 +24,23 @@ const months = [
 ];
 
 const setTime = () => {
-    const day = document.querySelector(".day");
-    const month = document.querySelector(".month");
-    const dayOfTheMonth = document.querySelector(".day-of-the-month");
+  const day = document.querySelector(".day");
+  const month = document.querySelector(".month");
+  const dayOfTheMonth = document.querySelector(".day-of-the-month");
 
-    const hours = document.querySelector(".hours");
-    const minutes = document.querySelector(".minutes");
-    const seconds = document.querySelector(".seconds");
+  const hours = document.querySelector(".hours");
+  const minutes = document.querySelector(".minutes");
+  const seconds = document.querySelector(".seconds");
 
-    const currentDate = new Date();
+  const currentDate = new Date();
 
-    day.innerText = week[currentDate.getDay()];
-    month.innerText = months[currentDate.getMonth()];
-    dayOfTheMonth.innerText = currentDate.getDate();
+  day.innerText = week[currentDate.getDay()];
+  month.innerText = months[currentDate.getMonth()];
+  dayOfTheMonth.innerText = currentDate.getDate();
 
-    hours.innerText = currentDate.getHours().toString().padStart(2, "0");
-    minutes.innerText = currentDate.getMinutes().toString().padStart(2, "0");
-    seconds.innerText = currentDate.getSeconds().toString().padStart(2, "0");
+  hours.innerText = currentDate.getHours().toString().padStart(2, "0");
+  minutes.innerText = currentDate.getMinutes().toString().padStart(2, "0");
+  seconds.innerText = currentDate.getSeconds().toString().padStart(2, "0");
 };
 
 setInterval(setTime, 1000);
-
